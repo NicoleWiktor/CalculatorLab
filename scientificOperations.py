85562531153e3c3475cd8dc591be943014b88767
benjansson <123697172+benjansson@users.noreply.github.com>
2023-02-01 19:48:50 -0500

    Update scientificOperations.py

--- scientificOperations.py
+++ scientificOperations.py
@@ -4,11 +4,9 @@ import math
 def scientific_calculate(operation, num1):
     """
     Performs the given scientific operation on the given number, which may be either an integer or a float.
-
     args:
         - operation: the operation to perform (sin, cos, tan, ln)
         - num1: the number to perform the operation on
-
     returns:
         - the result of the operation on the given number
     """
@@ -20,17 +18,17 @@ def scientific_calculate(operation, num1):
         # TODO: finish this statement
         # - should return the cosine of num1
         pass  # Replace this line with your code
-
+        return math.cos(num1)
     elif operation == 'tan':
         # TODO: finish this statement
         # - should return the tangent of num1
         pass  # Replace this line with your code
-
+        return math.tan(num1)
     elif operation == 'ln':
         # TODO: finish this statement
         # - should return the natural log of num1
         pass  # Replace this line with your code
-
+        return math.ln(num1)
     else:
         raise ValueError(
             'Invalid Operation: Scientific Operations are (sin, cos, tan, ln, sqrt, !, ^, and %)')
